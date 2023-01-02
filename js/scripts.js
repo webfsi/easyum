dfdeb50f496e20a270000daf5338db73a6ad4cdc
Mikhail Shishov <contact@michaelshishov.com>
2023-01-02 21:57:21 +0100

    02.01

--- js/scripts.js
+++ js/scripts.js
@@ -127,3 +127,28 @@ showMoreBtn?.addEventListener("click", () => {
     showMoreBtn.className = "d-none";
   }
 });
+
+// slider, reviews
+const swiperReviews = new Swiper('.swiper-reviews', {
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  }
+})
