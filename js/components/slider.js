7fe8946c6eeeb20d88bfe8e1c4bafe3c7a614089
ForestSpirit <forestspirit@email.cz>
2023-03-01 09:46:22 +0200

    fs inner

--- js/components/slider.js
+++ js/components/slider.js
@@ -78,4 +78,77 @@ export default () => {
 
 
 	});
+
+	const $slidersPersonal = document.querySelectorAll('[data-sliders-personal]');
+
+	$slidersPersonal.forEach((sliderEl) => {
+		const sInit = sliderEl.querySelector('.swiper_personal-cap'),
+			prev = sliderEl.querySelector('.swiper-button-prev'),
+			next = sliderEl.querySelector('.swiper-button-next'),
+			sInit2 = sliderEl.querySelector('.swiper_personal-info');
+			// pag = sliderEl.querySelector('.swiper-pagination');
+		let sliderMyOne = new Swiper(sInit, {
+			speed: 500,
+			// loop: true,
+
+			// observeParents: true,
+			// observeSlideChildren: true,
+			// observer: true,
+			// spaceBetween: 42,
+			slidesPerView: 1,
+
+			navigation: {
+				prevEl: prev,
+				nextEl: next,
+			},
+
+			// breakpoints: {
+			// 	1440: {
+			// 		slidesPerView: 3,
+			// 	},
+			// 	768: {
+			// 		slidesPerView: 3,
+			// 	},
+			// 	0: {
+			// 		slidesPerView: 2,
+			// 	},
+
+			// },
+		});
+
+		let sliderMyTwo = new Swiper(sInit2, {
+			speed: 500,
+			// loop: true,
+
+			// observeParents: true,
+			// observeSlideChildren: true,
+			// observer: true,
+			// spaceBetween: 42,
+			slidesPerView: 1,
+      effect: 'fade',
+
+			// navigation: {
+			// 	prevEl: prev,
+			// 	nextEl: next,
+			// },
+
+			// breakpoints: {
+			// 	1440: {
+			// 		slidesPerView: 3,
+			// 	},
+			// 	768: {
+			// 		slidesPerView: 3,
+			// 	},
+			// 	0: {
+			// 		slidesPerView: 2,
+			// 	},
+
+			// },
+		});
+
+		sliderMyOne.controller.control = sliderMyTwo;
+    sliderMyTwo.controller.control = sliderMyOne;
+
+
+	});
 }
